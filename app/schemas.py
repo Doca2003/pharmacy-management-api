e8cf3e8825e6d7937bdc67696e0de021473a52de
Doca2003 <dominique.guedes03@gmail.com>
2026-03-10 17:11:57 -0200

    Create schemas.py

---
+++ app/schemas.py
@@ -0,0 +1,47 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+#MEDICAMENTOS
+
+class MedicamentoCreate(BaseModel):
+    nome: str
+    quantidade:int
+    preco:float
+
+class MedicamentoResponse(BaseModel):
+    id:int
+    nome:str
+    quantidade:int
+    preco:float
+
+    class Config:
+        orm_mode=True
+
+
+#ITENS
+
+class ItemPedidoCreate(BaseModel):
+    medicamento_id:int
+    quantidade: int
+
+class ItemPedidoResponse(BaseModel):
+    id:int
+    medicamento_id:int
+    quantidade:int
+
+    class Config:
+        orm_mode = True
+        
+#PEDIDOS
+
+class PedidoCreate(BaseModel):
+    pass #nao envia nada ainda
+
+class PedidoResponse(BaseModel):
+    id:int
+    pedido_id:str
+    status:str
+    itens: List[ItemPedidoResponse] = []
+
+    class Config:
+        orm_mode = True
