eb878657cb2201ce9c5427d3a2cf40c3812c982b
Doca2003 <dominique.guedes03@gmail.com>
2026-03-11 16:27:50 -0200

    Update medicamentos.py

--- app/routers/medicamentos.py
+++ app/routers/medicamentos.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import Session
 from app.database import get_db
 from app.models import Medicamento
 from app.schemas import MedicamentoCreate, MedicamentoResponse
+from typing import List
+from datetime import datetime
 
 router = APIRouter(
     prefix="/medicamentos",
@@ -18,21 +20,25 @@ def criar_medicamento(med: MedicamentoCreate, db: Session = Depends(get_db)):
     return novo
 
 
-@router.get("/", response_model=list[MedicamentoResponse])
+@router.get("/", response_model=List[MedicamentoResponse])
 def listar_medicamentos(db: Session = Depends(get_db)):
     return db.query(Medicamento).all()
 
 
-@router.put("/{med_id}")
+@router.put("/{med_id}", response_model=MedicamentoResponse)
 def atualizar_quantidade(med_id: int, quantidade: int, db: Session = Depends(get_db)):
+
     med = db.query(Medicamento).filter(Medicamento.id == med_id).first()
 
     if not med:
         raise HTTPException(status_code=404, detail="Medicamento não encontrado no sistema")
 
     med.quantidade = quantidade
+
     db.commit()
-    return {"msg": "Atualizado"}
+    db.refresh(med)
+
+    return med
 
 
 @router.delete("/{med_id}")
@@ -45,3 +51,15 @@ def deletar_medicamento(med_id: int, db: Session = Depends(get_db)):
     db.delete(med)
     db.commit()
     return {"msg": "Removido"}
+
+    
+@router.get("/vencidos", response_model=List[MedicamentoResponse])
+def listar_medicamentos_vencidos(db: Session = Depends(get_db)):
+
+    hoje = datetime.now()
+
+    medicamentos_vencidos = db.query(Medicamento).filter(
+        Medicamento.validade < hoje
+    ).all()
+
+    return medicamentos_vencidos
