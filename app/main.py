f3b01202bb378078ed810a70607c76b06a1e2fbf
Doca2003 <dominique.guedes03@gmail.com>
2026-03-14 21:29:58 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from app.routers import medicamentos, pedidos
+from app.auth.auth_router import router as auth_router
+from app.routers import users_router
+
+from app.database import Base, engine
+from app import models
 
 app = FastAPI()
 
+# cria as tabelas
+Base.metadata.create_all(bind=engine)
+
 app.include_router(medicamentos.router)
 app.include_router(pedidos.router)
+app.include_router(auth_router)
+app.include_router(users_router.router)
