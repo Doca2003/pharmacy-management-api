8e5d8b9d772d403ebaaadabde25c41450fed37de
Doca2003 <dominique.guedes03@gmail.com>
2026-03-18 17:50:12 -0300

    Update users_router.py

--- app/routers/users_router.py
+++ app/routers/users_router.py
@@ -1,6 +1,10 @@
-from fastapi import APIRouter, Depends
-from app.auth.security import get_current_user
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app.auth.roles import RoleEnum
+from fastapi import APIRouter, Depends, HTTPException
+from app.auth.security import require_roles, get_current_user
 from app.models import Usuario
+from app.schemas import UpdateRoleSchema
 
 router = APIRouter(
     prefix="/users",
@@ -14,3 +18,20 @@ def read_me(usuario: Usuario = Depends(get_current_user)):
         "username": usuario.username,
         "role": usuario.role
     }
+
+@router.patch("/{user_id}/role")
+def alterar_role(
+    user_id: int,
+    data: UpdateRoleSchema,
+    db: Session = Depends(get_db),
+    admin: Usuario = Depends(require_roles(RoleEnum.admin))
+):
+    user = db.query(Usuario).filter(Usuario.id == user_id).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado")
+    
+    user.role = data.novo_role.value
+    db.commit()
+
+    return {"msg": f"Role de usuário {user.username} atualizado para {data.novo_role.value}"}
