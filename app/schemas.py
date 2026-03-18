601d5effdd48f457e0ec3679835f3b8621e3286e
Doca2003 <dominique.guedes03@gmail.com>
2026-03-18 17:49:07 -0300

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,28 +1,44 @@
 from pydantic import BaseModel
-from typing import List, Optional
+from typing import List
+from app.auth.roles import RoleEnum
 
-#MEDICAMENTOS
+
+# MEDICAMENTOS
 
 class MedicamentoCreate(BaseModel):
     nome: str
-    quantidade:int
-    preco:float
+    quantidade: int
+    preco: float
+
 
 class MedicamentoResponse(BaseModel):
-    id:int
-    nome:str
-    quantidade:int
-    preco:float
+    id: int
+    nome: str
+    quantidade: int
+    preco: float
 
     class Config:
-        from_attributes=True
+        from_attributes = True
 
 
-#ITENS
+class AtualizarEstoque(BaseModel):
+    quantidade: int
+
+
+class PaginatedMedicamentos(BaseModel):
+    page: int
+    limit: int
+    total: int
+    data: List[MedicamentoResponse]
+
+
+
+# ITENS DO PEDIDO
 
 class ItemPedidoCreate(BaseModel):
-    medicamento_id:int
+    medicamento_id: int
     quantidade: int
+
 
 class ItemPedidoResponse(BaseModel):
     id: int
@@ -32,24 +48,34 @@ class ItemPedidoResponse(BaseModel):
 
     class Config:
         from_attributes = True
-        
-#PEDIDOS
+
+
+
+# PEDIDOS
 
 class PedidoCreate(BaseModel):
-    pass #nao envia nada ainda
+    pass
+
 
 class PedidoResponse(BaseModel):
     id: int
     pedido_id: str
     status: str
-    valor_total: float
+    valor_total: float  #funciona com @property
     itens: List[ItemPedidoResponse] = []
 
     class Config:
         from_attributes = True
 
+class PaginatedPedidos(BaseModel):
+    page: int
+    limit: int
+    total: int
+    data: List[PedidoResponse]
 
-#USERS
+
+
+# USUÁRIOS / AUTH
 
 class UsuarioCreate(BaseModel):
     username: str
@@ -59,3 +85,7 @@ class UsuarioCreate(BaseModel):
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+
+class UpdateRoleSchema(BaseModel):
+    novo_role: RoleEnum
