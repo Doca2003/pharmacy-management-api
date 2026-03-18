4737ec0099aec96f82521fc4e81dbd8d733c78b1
Doca2003 <dominique.guedes03@gmail.com>
2026-03-18 17:49:30 -0300

    Update medicamentos.py

--- app/routers/medicamentos.py
+++ app/routers/medicamentos.py
@@ -1,11 +1,14 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 from app.database import get_db
-from app.models import Medicamento
-from app.schemas import MedicamentoCreate, MedicamentoResponse
+from app.models import Medicamento, Usuario
+from app.schemas import MedicamentoCreate, MedicamentoResponse, AtualizarEstoque, PaginatedMedicamentos
 from typing import List
-from datetime import datetime
-from app.auth.security import require_role
+from datetime import datetime, timezone
+from app.auth.security import require_roles
+from app.auth.roles import RoleEnum
+from sqlalchemy import asc, desc
+
 
 router = APIRouter(
     prefix="/medicamentos",
@@ -13,7 +16,7 @@ router = APIRouter(
 )
 
 @router.post("/", response_model=MedicamentoResponse)
-def criar_medicamento(med: MedicamentoCreate, db: Session = Depends(get_db), usuario: str = Depends(require_role("admin"))):
+def criar_medicamento(med: MedicamentoCreate, db: Session = Depends(get_db), usuario: Usuario = Depends(require_roles(RoleEnum.admin, RoleEnum.farmaceutico))):
     novo = Medicamento(**med.model_dump())
     db.add(novo)
     db.commit()
@@ -21,14 +24,58 @@ def criar_medicamento(med: MedicamentoCreate, db: Session = Depends(get_db), usu
     return novo
 
 
-@router.get("/", response_model=List[MedicamentoResponse])
-def listar_medicamentos(db: Session = Depends(get_db)):
-    return db.query(Medicamento).all()
+@router.get("/", response_model=PaginatedMedicamentos)
+def listar_medicamentos(
+    nome: str = Query(None),
+    estoque_min: int = Query(None),
+    sort_by: str = Query("nome"),
+    order: str = Query("asc"),
+    page: int = Query(1, ge=1),
+    limit: int = Query(10, ge=1, le=100),
+    db: Session = Depends(get_db)
+):
+    query = db.query(Medicamento)
+
+    #filtros
+
+    if nome:
+        query = query.filter(Medicamento.nome.ilike(f"%{nome}%"))
+
+    if estoque_min is not None:
+        query = query.filter(Medicamento.quantidade >= estoque_min)
+
+    #ordenacao
+    if sort_by == "nome":
+        coluna = Medicamento.nome
+    elif sort_by == "preco":
+        coluna = Medicamento.preco
+    elif sort_by == "quantidade":
+        coluna = Medicamento.quantidade
+    else:
+        coluna = Medicamento.nome
+
+    if order == "desc":
+        query = query.order_by(desc(coluna))
+    else:
+        query = query.order_by(asc(coluna))
+
+    #pagniacao
+    total = query.count()
+    offset = (page - 1) * limit
+    medicamentos = query.offset(offset).limit(limit).all()
+
+    return {
+        "page": page,
+        "limit": limit,
+        "total": total,
+        "data": medicamentos
+    }
 
 
 @router.put("/{med_id}", response_model=MedicamentoResponse)
-def atualizar_quantidade(med_id: int, quantidade: int, db: Session = Depends(get_db)):
+def atualizar_quantidade(med_id: int,data: AtualizarEstoque, quantidade: int, db: Session = Depends(get_db), usuario: Usuario = Depends(require_roles(RoleEnum.admin,RoleEnum.admin))):
 
+    med.quantidade = data.quantidade
     med = db.query(Medicamento).filter(Medicamento.id == med_id).first()
 
     if not med:
@@ -43,7 +90,7 @@ def atualizar_quantidade(med_id: int, quantidade: int, db: Session = Depends(get
 
 
 @router.delete("/{med_id}")
-def deletar_medicamento(med_id: int, db: Session = Depends(get_db), usuario: str = Depends(require_role("admin"))):
+def deletar_medicamento(med_id: int, db: Session = Depends(get_db), usuario: Usuario = Depends(require_roles(RoleEnum.admin))):
     med = db.query(Medicamento).filter(Medicamento.id == med_id).first()
 
     if not med:
@@ -55,9 +102,9 @@ def deletar_medicamento(med_id: int, db: Session = Depends(get_db), usuario: str
 
     
 @router.get("/vencidos", response_model=List[MedicamentoResponse])
-def listar_medicamentos_vencidos(db: Session = Depends(get_db)):
+def listar_medicamentos_vencidos(db: Session = Depends(get_db), usuario: Usuario = Depends(require_roles(RoleEnum.admin,RoleEnum.farmaceutico))):
 
-    hoje = datetime.now()
+    hoje = datetime.now(timezone.utc)
 
     medicamentos_vencidos = db.query(Medicamento).filter(
         Medicamento.validade < hoje
